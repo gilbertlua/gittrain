19e47106ce49fbeba6ec56e445f165f7d6629283
gilbertlua <gilbertgmtidn@gmail.com>
2023-05-15 10:12:09 +0700

    commit terbaru

---
+++ cfile.c
@@ -0,0 +1,3 @@
+ini adalah bagian dari yang saya kerjakan pada masa lampau dan akan coba saya kerjakan lagi di masa sekarang
+
+yohanes 3:16 karena begitu besar kasih Allah akan dunia ini sehingga ia telah mengaruniakan anaknya yang tunggal supaya setiap orang yang percaya padanya tidak binasa melainkan beroleh hidup yang kekal
